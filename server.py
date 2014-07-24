0ff04c91a822d0e273a7e379e2739812833d7a45
friedrich groestlinger <friedrich.groestlinger@gmail.com>
2014-07-24 23:45:04 +0200

    deleted some commentary

--- server.py
+++ server.py
@@ -43,18 +43,3 @@ class Server(object):
 if __name__ == '__main__':
     s = Server('', 50011)
     s.start()
-
-
-
-#HOST = ''
-#PORT = 50010
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((HOST, PORT))
-#s.listen(1)
-#conn, addr = s.accept()
-#print('Connected by', addr)
-#while 1:
-#    data = conn.recv(1024)
-#    if not data: break
-#    conn.sendall(data)
-#conn.close()
